0c2f37d308683935fa0a50a052abbd4272694807
ThoLe <thole020287@gmail.com>
2017-08-13 22:38:14 +0200

    Date Sun Aug 13 22:38:14 CEST 2017

--- tutorial/spiders/myweb.py
+++ tutorial/spiders/myweb.py
@@ -19,7 +19,7 @@ import pandas as pd
 
 Max_depth=2
 filters_scheme=r'^(javascript|mailto|tel)'
-filters_pages=r'.*?(facebook|twitter|linkedin|youtube|yahoo|Flickr|apple|google|vimeo|instagram).*?'
+filters_pages=r'.*?(facebook|twitter|linkedin|youtube|yahoo|Flickr|apple|google|vimeo|instagram|cie|ibo|BBC|news|newsletter|calendar|job).*?'
 filters_extension=r'(\.pdf|\.jpg|\.mp4|\.png|\.jpeg|\.icon|\.doc|\.docx|\.xls|\.xlsx|\.js|\.bz2|rss|RSS)$'
 
 regex_schemes=re.compile(filters_scheme,re.IGNORECASE)
@@ -41,11 +41,13 @@ class myspider(scrapy.Spider):
             request.meta['previous']=url
             yield request
     def parse_web(self, response):
-        filter_title=r'(?i)user|username|password|platform|connect|portal|admission|student|parent|parental|log in|login|sign in|signin|apply|enrol|SIS|SIMS|MIS|information|staff|sign up|powerschool|ISAM|Engage'
-        filter_body=r'(?i)\buser\b|\busername\b|\bpassword\b|\bplatform\b|\bportal\b|\blog in\b|\blogin\b|\bsign in\b|\bsignin\b|\bSIS\b|\bSIMS\b|\bMIS\b|\bsign up\b|\bpowerschool\b|\bISAM\b|\bEngage\b'
+        filter_exclude=r'(?i)Newsletter|Calendar|summary|music|policy|Mr|Mrs|news|blog|Curriculum|summer|jobs|contact|arts'
+        filter_title=r'(?i)user|username|password|platform|connect|portal|admission|student|parent|parental|log in|login|sign in|signin|apply|enroll|SIS|SIMS|MIS|sign up|powerschool|ISAM|Engage|Veracross|myschoolportal|SchoolBase'
+        filter_body=r'(?i)\buser\b|\busername\b|\bpassword\b|\bplatform\b|\bportal\b|\blog in\b|\blogin\b|\bsign in\b|\bsignin\b|\bSIS\b|\bSIMS\b|\bMIS\b|\bsign up\b|\bpowerschool\b|\bISAM\b|\bEngage\b|\bVeracross\b|\bmyschoolportal\b|\bSchoolBase\b'
         title=response.xpath('//title/text()').re(filter_title)
         body=response.xpath('//body//text()').re(filter_body)
-        if len(title)>0 or len(body)>0:
+        body_exclude=response.xpath('//body//text()').re(filter_exclude)
+        if (len(body_exclude)==0) and (len(title)>0 or len(body)>0):
             l = ItemLoader(item=SchoolInfo(), response=response)
             l.add_css('title',"title::text")
             l.add_value('depth',response.meta['depth'])
